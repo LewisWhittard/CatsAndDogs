3951aa68843484fe827d5f83c10a420342214506
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2023-08-05 19:42:47 +0100

    non data unit tests

--- CatsAndDogs/Data/Repository/BaseRepository.cs
+++ CatsAndDogs/Data/Repository/BaseRepository.cs
@@ -8,28 +8,28 @@ namespace CatsAndDogs.Data.Repository
 {
 	public class BaseRepository
 	{
-		protected void ClearTXT(string additionalString)
+		public void ClearTXT(string additionalString)
 		{
 			string BasePath = System.IO.Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory);
 			String FullPath = BasePath + additionalString;
 			File.WriteAllText(FullPath, String.Empty);
 		}
 
-		protected string FilteredString(string Data)
+		public string FilteredString(string Data)
 		{
 			var newResult = Data.Replace("<Completed>", string.Empty);
-			newResult = Data.Replace("\r\n", string.Empty);
+			newResult = newResult.Replace("\r\n", string.Empty);
 			return newResult;
 		}
 
-		protected List<string> SplitRows(string Data)
+		public List<string> SplitRows(string Data)
 		{
 			List<string> Content = Data.Split("<DataRow>").ToList();
 			Content.RemoveAt(Content.Count - 1);
 			return Content;
 		}
 
-		protected string GetData(String AdditionalPath)
+		public string GetData(String AdditionalPath)
 		{
 			string BasePath = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory);
 			string FullPath = BasePath + AdditionalPath;
--- UnitTest/BaseRepositoryTests.cs
+++ UnitTest/BaseRepositoryTests.cs
@@ -1,29 +1,31 @@
+using CatsAndDogs.Data.Repository;
+using System.IO;
 namespace UnitTest
 {
     public class BaseRepositoryTests
     {
         [Fact]
-        public void ClearTXT()
-        {
-            
-        }
-
-        [Fact]
         public void FilteredString()
         {
-           
+            BaseRepository BR = new BaseRepository();
+            var testData = BR.FilteredString("<Completed>test\r\n<Completed>");
+            bool Value = testData == "test";
+            Assert.True(Value);
         }
 
         [Fact]
         public void SplitRows()
         {
-            
+            BaseRepository BR = new BaseRepository();
+            var testData = BR.SplitRows("Test0<DataRow>Test1<DataRow>Test2<DataRow>");
+            bool Row0 = testData[0] == "Test0";
+            bool Row1 = testData[1] == "Test1";
+            bool Row2 = testData[2] == "Test2";
+            Assert.True(Row0);
+            Assert.True(Row1);
+            Assert.True(Row2);
         }
 
-        [Fact]
-        public void GetData()
-        {
 
-        }
     }
 }
