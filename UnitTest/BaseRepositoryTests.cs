088dc2a8b853984eda173bc9e47cb231c0f8f149
LewisWhittard <63914200+LewisWhittard@users.noreply.github.com>
2023-08-05 16:02:31 +0100

    unit tests config

---
+++ UnitTest/BaseRepositoryTests.cs
@@ -0,0 +1,29 @@
+namespace UnitTest
+{
+    public class BaseRepositoryTests
+    {
+        [Fact]
+        public void ClearTXT()
+        {
+            
+        }
+
+        [Fact]
+        public void FilteredString()
+        {
+           
+        }
+
+        [Fact]
+        public void SplitRows()
+        {
+            
+        }
+
+        [Fact]
+        public void GetData()
+        {
+
+        }
+    }
+}
